9fd819a45e49941cadf4a9757cbb77a2c026f338
KamilaKonusheva <67046552+KamilaKonusheva@users.noreply.github.com>
2020-10-12 17:25:46 +0300

    Update User.java

--- src/main/java/lesson1/User.java
+++ src/main/java/lesson1/User.java
@@ -6,5 +6,4 @@ public class User extends MainEntity {
     public User(String name, String surname, int age, String mail, String password, String role) {
         super(name, surname, age, mail, password, "USER");
     }
-
 }
