80de344629814467110733d82f8b942c3c22852a
KamilaKonusheva <67046552+KamilaKonusheva@users.noreply.github.com>
2020-10-12 17:25:23 +0300

    Update Service.java

--- src/main/java/lesson1/Service.java
+++ src/main/java/lesson1/Service.java
@@ -3,9 +3,9 @@ package lesson1;
 import java.io.IOException;
 
 public interface Service {
-     boolean userCheck(MainEntity mainEntity) throws IOException;
-
-    void userInfoWriter(MainEntity mainEntity)throws IOException;
-    }
+     
+    boolean userCheck(MainEntity mainEntity) throws IOException;
+    void userInfoWriter(MainEntity mainEntity)throws IOException; 
+}
 
 
